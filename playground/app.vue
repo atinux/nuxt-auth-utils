75100f6b991d5cd8a4f468eacb7c16dc1d8a670e
Arash <38922203+arashsheyda@users.noreply.github.com>
2023-11-11 07:39:22 -0700

    feat: refactor login buttons to use dropdown

--- playground/app.vue
+++ playground/app.vue
@@ -1,60 +1,65 @@
-<script setup>
+<script setup lang="ts">
 const { loggedIn, session, clear } = useUserSession()
+
+const providers = computed(() => [
+  [
+    {
+      label: !session.value.user?.github ? 'Github' : session.value.user.github.login,
+      to: !session.value.user?.github ? '/auth/github' : undefined,
+      disabled: session.value.user?.github && true,
+      icon: 'i-simple-icons-github',
+      prefetch: false,
+      external: true,
+    },
+    {
+      label: !session.value.user?.spotify ? 'Spotify' : session.value.user.spotify.display_name,
+      to: !session.value.user?.spotify ? '/auth/spotify' : undefined,
+      disabled: session.value.user?.spotify && true,
+      icon: 'i-simple-icons-spotify',
+      prefetch: false,
+      external: true,
+    },
+    {
+      label: !session.value.user?.google ? 'Google' : session.value.user.google.email,
+      to: !session.value.user?.google ? '/auth/google' : undefined,
+      disabled: session.value.user?.google && true,
+      icon: 'i-simple-icons-google',
+      prefetch: false,
+      external: true,
+    },
+    {
+      label: !session.value.user?.twitch ? 'Twitch' : session.value.user.twitch.login,
+      to: !session.value.user?.twitch ? '/auth/twitch' : undefined,
+      disabled: session.value.user?.twitch && true,
+      icon: 'i-simple-icons-twitch',
+      prefetch: false,
+      external: true,
+    },
+    {
+      label: !session.value.user?.auth0 ? 'Auth0' : session.value.user.auth0.email,
+      to: !session.value.user?.auth0 ? '/auth/auth0' : undefined,
+      disabled: session.value.user?.auth0 && true,
+      icon: 'i-simple-icons-auth0',
+      prefetch: false,
+      external: true,
+    },
+  ],
+])
 </script>
 
 <template>
   <UHeader>
     <template #right>
-      <UButton
-        v-if="!loggedIn || !session.user.github"
-        to="/auth/github"
-        icon="i-simple-icons-github"
-        external
-        color="gray"
-        size="xs"
-      >
-        Login with GitHub
-      </UButton>
-      <UButton
-        v-if="!loggedIn || !session.user.spotify"
-        to="/auth/spotify"
-        icon="i-simple-icons-spotify"
-        external
-        color="gray"
-        size="xs"
-      >
-        Login with Spotify
-      </UButton>
-      <UButton
-        v-if="!loggedIn || !session.user.google"
-        to="/auth/google"
-        icon="i-simple-icons-google"
-        external
-        color="gray"
-        size="xs"
-      >
-        Login with Google
-      </UButton>
-      <UButton
-        v-if="!loggedIn || !session.user.twitch"
-        to="/auth/twitch"
-        icon="i-simple-icons-twitch"
-        external
-        color="gray"
-        size="xs"
-      >
-        Login with Twitch
-      </UButton>
-      <UButton
-        v-if="!loggedIn || !session.user.auth0"
-        to="/auth/auth0"
-        icon="i-simple-icons-auth0"
-        external
-        color="gray"
-        size="xs"
-      >
-        Login with Auth0
-      </UButton>
+      <UDropdown :items="providers">
+        <UButton
+          icon="i-heroicons-chevron-down"
+          trailing
+          color="gray"
+          size="xs"
+        >
+          Login with
+        </UButton>
+      </UDropdown>
       <UButton
         v-if="loggedIn"
         color="gray"
