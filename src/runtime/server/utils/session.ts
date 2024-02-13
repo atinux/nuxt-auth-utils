eeed7c563ed0c31aa0a8c2152c48fee25132e272
Sébastien Chopin <seb@nuxt.com>
2024-02-13 16:02:30 +0100

    chore: update

--- src/runtime/server/utils/session.ts
+++ src/runtime/server/utils/session.ts
@@ -44,6 +44,7 @@ export async function setUserSession (event: H3Event, data: UserSession) {
 export async function replaceUserSession (event: H3Event, data: UserSession) {
   const session = await _useSession(event)
 
+  await session.clear()
   await session.update(data)
 
   return session.data
