01dd56c551d8683af3813764ded5808e9b04337f
Brendon Matos <brendonferreiradm@gmail.com>
2024-03-22 16:01:17 -0300

    Atinux
    * refactor: userInfoURL from google provider to be configurable
    
    * chore: rename to userURL
    
    ---------
    
    Co-authored-by: Sébastien Chopin <seb@nuxt.com>

--- src/runtime/server/lib/oauth/google.ts
+++ src/runtime/server/lib/oauth/google.ts
@@ -46,6 +46,12 @@ export interface OAuthGoogleConfig {
   tokenURL?: string;
 
   /**
+   * Google OAuth User URL
+   * @default 'https://www.googleapis.com/oauth2/v3/userinfo'
+   */
+  userURL?: string;
+
+  /**
    * Extra authorization parameters to provide to the authorization URL
    * @see https://developers.google.com/identity/protocols/oauth2/web-server#httprest_3
    * @example { access_type: 'offline' }
@@ -63,6 +69,7 @@ export function googleEventHandler({
     config = defu(config, useRuntimeConfig(event).oauth?.google, {
       authorizationURL: 'https://accounts.google.com/o/oauth2/v2/auth',
       tokenURL: 'https://oauth2.googleapis.com/token',
+      userURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
       authorizationParams: {}
     }) as OAuthGoogleConfig
     const { code } = getQuery(event)
@@ -119,7 +126,7 @@ export function googleEventHandler({
 
     const accessToken = tokens.access_token
     const user: any = await ofetch(
-      'https://www.googleapis.com/oauth2/v3/userinfo',
+      config.userURL as string,
       {
         headers: {
           Authorization: `Bearer ${accessToken}`,
