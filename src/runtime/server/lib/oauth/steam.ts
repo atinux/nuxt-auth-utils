38f847a71bbeb2905c1bb34031bd5307a41be250
Ahmed Rangel <ahmedrangel@outlook.com>
2024-06-29 01:53:57 -0500

    chore: move redirect url and openid params inside if

--- src/runtime/server/lib/oauth/steam.ts
+++ src/runtime/server/lib/oauth/steam.ts
@@ -36,16 +36,15 @@ export function steamEventHandler({ config, onSuccess, onError }: OAuthConfig<OA
       return onError(event, error)
     }
 
-    const redirectUrl = getRequestURL(event).href
-    const steamOpenIdParams = {
-      'openid.ns': 'http://specs.openid.net/auth/2.0',
-      'openid.mode': 'checkid_setup',
-      'openid.return_to': redirectUrl,
-      'openid.identity': 'http://specs.openid.net/auth/2.0/identifier_select',
-      'openid.claimed_id': 'http://specs.openid.net/auth/2.0/identifier_select',
-    };
-
     if (!query['openid.claimed_id']) {
+      const redirectUrl = getRequestURL(event).href
+      const steamOpenIdParams = {
+        'openid.ns': 'http://specs.openid.net/auth/2.0',
+        'openid.mode': 'checkid_setup',
+        'openid.return_to': redirectUrl,
+        'openid.identity': 'http://specs.openid.net/auth/2.0/identifier_select',
+        'openid.claimed_id': 'http://specs.openid.net/auth/2.0/identifier_select',
+      };
       // Redirect to Steam Oauth page
       return sendRedirect(event, withQuery(config.authorizationURL as string, steamOpenIdParams))
     }
