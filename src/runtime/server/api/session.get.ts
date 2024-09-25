3bd9c3b7cba9103bed7a1200038e1adc92afe989
Sébastien Chopin <seb@nuxt.com>
2024-09-25 14:54:21 +0200

    feat!: call `fetch` hook if session is not empty instead of user defined (#188)

--- src/runtime/server/api/session.get.ts
+++ src/runtime/server/api/session.get.ts
@@ -5,7 +5,8 @@ import type { UserSessionRequired } from '#auth-utils'
 export default eventHandler(async (event) => {
   const session = await getUserSession(event)
 
-  if (session.user) {
+  // If session is not empty, call fetch hook
+  if (Object.keys(session).length > 0) {
     await sessionHooks.callHookParallel('fetch', session as UserSessionRequired, event)
   }
 
