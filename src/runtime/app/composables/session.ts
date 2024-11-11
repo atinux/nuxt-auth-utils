065c54dba8e2b06b356de421bca4a91881158a4f
Sébastien Chopin <seb@nuxt.com>
2024-11-11 17:08:16 +0100

    fix(composable): use same context for `clear` and `fetch`
    Resolves #273

--- src/runtime/app/composables/session.ts
+++ src/runtime/app/composables/session.ts
@@ -1,16 +1,31 @@
 import { useState, computed, useRequestFetch } from '#imports'
 import type { UserSession, UserSessionComposable } from '#auth-utils'
 
-const useSessionState = () => useState<UserSession>('nuxt-session', () => ({}))
-const useAuthReadyState = () => useState('nuxt-auth-ready', () => false)
-
 /**
  * Composable to get back the user session and utils around it.
  * @see https://github.com/atinux/nuxt-auth-utils
  */
 export function useUserSession(): UserSessionComposable {
-  const sessionState = useSessionState()
-  const authReadyState = useAuthReadyState()
+  const sessionState = useState<UserSession>('nuxt-session', () => ({}))
+  const authReadyState = useState('nuxt-auth-ready', () => false)
+
+  const clear = async () => {
+    await $fetch('/api/_auth/session', { method: 'DELETE' })
+    sessionState.value = {}
+  }
+
+  const fetch = async () => {
+    sessionState.value = await useRequestFetch()('/api/_auth/session', {
+      headers: {
+        Accept: 'text/json',
+      },
+      retry: false,
+    }).catch(() => ({}))
+    if (!authReadyState.value) {
+      authReadyState.value = true
+    }
+  }
+
   return {
     ready: computed(() => authReadyState.value),
     loggedIn: computed(() => Boolean(sessionState.value.user)),
@@ -19,22 +34,4 @@ export function useUserSession(): UserSessionComposable {
     fetch,
     clear,
   }
-}
-
-async function fetch() {
-  const authReadyState = useAuthReadyState()
-  useSessionState().value = await useRequestFetch()('/api/_auth/session', {
-    headers: {
-      Accept: 'text/json',
-    },
-    retry: false,
-  }).catch(() => ({}))
-  if (!authReadyState.value) {
-    authReadyState.value = true
-  }
-}
-
-async function clear() {
-  await $fetch('/api/_auth/session', { method: 'DELETE' })
-  useSessionState().value = {}
 }
