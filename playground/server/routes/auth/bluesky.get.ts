e414bd53a2b3ae89369cf3a74d2e31dc10b96ae7
Neil <me@neilrichter.com>
2024-11-14 00:30:10 +0100

    feat(playground): add bluesky to playground

---
+++ playground/server/routes/auth/bluesky.get.ts
@@ -0,0 +1,12 @@
+export default defineOAuthBlueskyEventHandler({
+  async onSuccess(event, { user }) {
+    await setUserSession(event, {
+      user: {
+        bluesky: user?.did,
+      },
+      loggedInAt: Date.now(),
+    })
+
+    return sendRedirect(event, '/')
+  },
+})
