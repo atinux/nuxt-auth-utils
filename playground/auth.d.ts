f147d1a8074efbd40ebcf0c0a14585a769327cd8
Sébastien Chopin <seb@nuxt.com>
2024-11-14 11:23:32 +0100

    feat: forward set-cookie header for `useUserSession().clear()` (#282)
    * feat: forward set-cookie header for `useUserSession().clear()`
    
    * chore: update

--- playground/auth.d.ts
+++ playground/auth.d.ts
@@ -34,8 +34,14 @@ declare module '#auth-utils' {
   interface UserSession {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     extended?: any
+    jtw?: {
+      accessToken: string
+      refreshToken: string
+    }
     loggedInAt: number
-    secure?: Record<string, unknown>
+  }
+
+  interface SecureSessionData {
   }
 }
 
--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -9,14 +9,24 @@
           ...
         </template>
       </AuthState>
-      <UButton
-        to="/secret"
-        class="mt-2"
-        variant="link"
-        :padded="false"
-      >
-        Secret page
-      </UButton>
+      <div class="flex flex-col gap-2 mt-4">
+        <UButton
+          to="/secret"
+          class="mt-2"
+          variant="link"
+          :padded="false"
+        >
+          Secret page
+        </UButton>
+        <UButton
+          to="/about"
+          class="mt-2"
+          variant="link"
+          :padded="false"
+        >
+          About page
+        </UButton>
+      </div>
     </UPageBody>
   </UPage>
 </template>
