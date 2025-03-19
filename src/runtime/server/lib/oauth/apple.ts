c7ce610ad900acf8e44c1d41ba8dc6718202bfbe
Dominik Opyd <dominik.opyd@gmail.com>
2025-03-19 11:51:03 +0100

    fix(oauth, apple): return tokens in response (#373)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- src/runtime/server/lib/oauth/apple.ts
+++ src/runtime/server/lib/oauth/apple.ts
@@ -162,17 +162,17 @@ export function defineOAuthAppleEventHandler({
         },
       })
 
-      const tokens = await verifyJwt<OAuthAppleTokens>(accessTokenResult.id_token, {
+      const payload = await verifyJwt<OAuthAppleTokens>(accessTokenResult.id_token, {
         publicJwkUrl: 'https://appleid.apple.com/auth/keys',
         audience: config.clientId,
         issuer: 'https://appleid.apple.com',
       })
 
-      if (!tokens) {
-        return handleAccessTokenErrorResponse(event, 'apple', tokens, onError)
+      if (!payload) {
+        return handleAccessTokenErrorResponse(event, 'apple', payload, onError)
       }
 
-      return onSuccess(event, { user, tokens })
+      return onSuccess(event, { user, payload, tokens: accessTokenResult })
     }
     catch (error) {
       return handleAccessTokenErrorResponse(event, 'apple', error, onError)
