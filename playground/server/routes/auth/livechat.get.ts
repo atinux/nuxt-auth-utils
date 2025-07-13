978f23986fd4f89b2bcba281b67a02c52213b792
Marios Antonoudiou <mariosant@sent.com>
2025-07-13 12:20:10 +0300

    Lint

--- playground/server/routes/auth/livechat.get.ts
+++ playground/server/routes/auth/livechat.get.ts
@@ -2,7 +2,7 @@ export default defineOAuthLiveChatEventHandler({
   async onSuccess(event, { user }) {
     await setUserSession(event, {
       user: {
-        livechat: user.name
+        livechat: user.name,
       },
       loggedInAt: Date.now(),
     })
